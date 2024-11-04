3daf2c3dc6f5e776af9eeac0ffa51263ada22b7f
Hema Bindra <hbindra11@yahoo.com>
2024-11-04 12:34:02 +0100

    fixed issue with post by adding a header with token

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -4,7 +4,14 @@ import axios from "axios";
 
 const CreateEvent = () => {
   const isAuthenticated = localStorage.getItem("token") || null;
-  const [formData, setFormData] = useState({title:""});
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    date: "",
+    location: "",
+    latitude: "8.404746955649602",
+    longitude: "49.01438194665317",
+  });
 
   const handelChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -13,8 +20,11 @@ const CreateEvent = () => {
   const handelSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+    console.log(isAuthenticated);
     axios
-      .post("http://localhost:3001/api/events", formData)
+      .post("http://localhost:3001/api/events", formData, {
+        headers: { Authorization: `Bearer ${isAuthenticated}` }
+      })
       .then((res) => {
         console.log("response data: " + res.data);
         alert("The event was saved successfully!");
